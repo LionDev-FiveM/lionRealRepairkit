818f02c435e1a198c67819a85c5da63c9bda7d1b
Lion Development <157517442+LionDev-FiveM@users.noreply.github.com>
2024-01-24 21:34:33 +0100

    Add files via upload

--- server.lua
+++ server.lua
@@ -19,13 +19,13 @@ Citizen.CreateThread(function()
     local vRaw = LoadResourceFile(GetCurrentResourceName(), 'version.json')
     if vRaw and Config.VersionCheck then
         local v = json.decode(vRaw)
-        local url = 'https://raw.githubusercontent.com/LionDev-FiveM/lionDRP/main/lionDRP/version.json'
+        local url = 'https://raw.githubusercontent.com/LionDev-FiveM/lionRealRepairkit/main/version.json'
         PerformHttpRequest(url, function(code, res)
             if code == 200 then
                 local rv = json.decode(res)
                 if rv.version ~= v.version then
                     print(([[
-_      _               _____                 _                                  _   
+ _      _               _____                 _                                  _   
 | |    (_)             |  __ \               | |                                | |  
 | |     _  ___  _ __   | |  | | _____   _____| | ___  _ __  _ __ ___   ___ _ __ | |_ 
 | |    | |/ _ \| '_ \  | |  | |/ _ \ \ / / _ \ |/ _ \| '_ \| '_ ` _ \ / _ \ '_ \| __|
@@ -36,7 +36,7 @@ _      _               _____                 _                                  
 Script: lionRealRepairkit
 UPDATE: %s AVAILABLE
 CHANGELOG: %s
-Download: https://github.com/LionDev-FiveM/RealRepairkit
+Download: https://github.com/LionDev-FiveM/lionRealRepairkit
 If you need help, join our Discord: https://dsc.gg/lion-dev
 ]]):format(rv.version, rv.changelog))
                 end

db4f5d412d0769d69f4bf47b838917c3632e96c9
Lion Development <157517442+LionDev-FiveM@users.noreply.github.com>
2024-01-24 21:24:19 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config						= {}
+
+
+Config.RepairTime = 10 --in seconds | Repair time
+
+Config.OnlyOutside = "You can only use the repair kit from outside this vehicle!"
+Config.PlsWait = "Please wait..."
+Config.Successfull = "You have repaired your Engine! :D"
+Config.NotNear = "There is no car arround you"
+
+Config.Notify = "okok" --you have the choise of: esx, okok or pnotify
+Config.NotifyTitle = "Vehicle" --Notify title ||| only if you use okok notify
+Config.NotifyTimeout = 6 --in seconds | Notify Timeout
+Config.Layout = "topright" --notify layout
+Config.Type = "success" --notify type
+
+Config.VersionCheck = true

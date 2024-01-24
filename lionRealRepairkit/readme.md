b7e44f2a3096a5745cc8bec0d537f80294da8ec2
Lion Development <157517442+LionDev-FiveM@users.noreply.github.com>
2024-01-24 22:03:22 +0100

    Add files via upload

---
+++ lionRealRepairkit/readme.md
@@ -0,0 +1,18 @@
+[lionRealRepairkit]
+
+[Features]
+- Realistic
+- 3 notifications(okok, pnotify, esx)
+- Only outsite the car
+- Open Source
+
+
+[Installation]
+1. Drag & Drop the Script to your server
+2. Open your Server.cfg / CFG Editor in TxAdmin
+3. Add "ensure lionRealRepairkit" to your cfg
+4. edit the config as you want
+5. Enjoy it!
+
+[Support]
+Discord: https://dsc.gg/lion-dev
---
+++ lionRealRepairkit/server.lua
@@ -0,0 +1,48 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) 
+	ESX = obj 
+end)
+
+ESX.RegisterUsableItem('repairkit', function(playerId)
+	TriggerClientEvent('zvRealRepairkit:useRepKit', playerId)
+end)
+
+RegisterNetEvent('zvRealRepairkit:removeItem')
+AddEventHandler('zvRealRepairkit:removeItem', function()
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	xPlayer.removeInventoryItem('repairkit', 1)
+end)
+
+Citizen.CreateThread(function()
+    local vRaw = LoadResourceFile(GetCurrentResourceName(), 'version.json')
+    if vRaw and Config.VersionCheck then
+        local v = json.decode(vRaw)
+        local url = 'https://raw.githubusercontent.com/LionDev-FiveM/lionRealRepairkit/main/version.json'
+        PerformHttpRequest(url, function(code, res)
+            if code == 200 then
+                local rv = json.decode(res)
+                if rv.version ~= v.version then
+                    print(([[
+ _      _               _____                 _                                  _   
+| |    (_)             |  __ \               | |                                | |  
+| |     _  ___  _ __   | |  | | _____   _____| | ___  _ __  _ __ ___   ___ _ __ | |_ 
+| |    | |/ _ \| '_ \  | |  | |/ _ \ \ / / _ \ |/ _ \| '_ \| '_ ` _ \ / _ \ '_ \| __|
+| |____| | (_) | | | | | |__| |  __/\ V /  __/ | (_) | |_) | | | | | |  __/ | | | |_ 
+|______|_|\___/|_| |_| |_____/ \___| \_/ \___|_|\___/| .__/|_| |_| |_|\___|_| |_|\__|
+                                                    | |                             
+                                                    |_|                             
+Script: lionRealRepairkit
+UPDATE: %s AVAILABLE
+CHANGELOG: %s
+Download: https://github.com/LionDev-FiveM/lionRealRepairkit
+If you need help, join our Discord: https://dsc.gg/lion-dev
+]]):format(rv.version, rv.changelog))
+                end
+            else
+                print('Error in versioncheck')
+            end
+        end, 'GET')
+    end
+end)
